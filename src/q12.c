7a0775f0afcb02c29b2c7568b451598d342da6a8
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-18 22:38:15 +0545

    Update q12.c

--- src/q12.c
+++ src/q12.c
@@ -1,1 +1,16 @@
-// Write a C program to read the contents of a text file and display them on the screen.
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+
+    fp = fopen("output.txt", "r");
+    if(fp == NULL) return 1;
+
+    while((ch = fgetc(fp)) != EOF) {
+        putchar(ch);
+    }
+
+    fclose(fp);
+    return 0;
+}
