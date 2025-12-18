d11ab8be5b4d394864f83c02102fa17041958bf2
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-18 22:35:29 +0545

    Update q10.c

--- src/q10.c
+++ src/q10.c
@@ -1,2 +1,25 @@
-// Write a C program that defines a structure Car with members: model, year, and mileage. 
-// Store information for 3 cars and find the car with the lowest mileage.
+#include <stdio.h>
+#include <string.h>
+
+struct Car {
+    char model[50];
+    int year;
+    int mileage;
+};
+
+int main() {
+    struct Car cars[3];
+    for(int i = 0; i < 3; i++) {
+        scanf("%s %d %d", cars[i].model, &cars[i].year, &cars[i].mileage);
+    }
+
+    int minIndex = 0;
+    for(int i = 1; i < 3; i++) {
+        if(cars[i].mileage < cars[minIndex].mileage) {
+            minIndex = i;
+        }
+    }
+
+    printf("%s %d %d\n", cars[minIndex].model, cars[minIndex].year, cars[minIndex].mileage);
+    return 0;
+}
