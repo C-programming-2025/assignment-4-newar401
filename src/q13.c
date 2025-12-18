4abae71becc7c4f9366cbca09471e002ea7bfe41
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-18 22:38:50 +0545

    Update q13.c

--- src/q13.c
+++ src/q13.c
@@ -1,1 +1,21 @@
-// Write a C program that copies the contents of one file to another file.
+#include <stdio.h>
+
+int main() {
+    FILE *source, *dest;
+    char ch;
+
+    source = fopen("source.txt", "r");
+    if(source == NULL) return 1;
+
+    dest = fopen("destination.txt", "w");
+    if(dest == NULL) return 1;
+
+    while((ch = fgetc(source)) != EOF) {
+        fputc(ch, dest);
+    }
+
+    fclose(source);
+    fclose(dest);
+    printf("File copied successfully\n");
+    return 0;
+}
