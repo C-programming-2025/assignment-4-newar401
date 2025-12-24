0c8a183dc3362473a672f7ba0750cea25505e31f
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-24 09:02:13 +0545

    Update q5.c

--- src/q5.c
+++ src/q5.c
@@ -1,1 +1,29 @@
-// Write a C program that dynamically allocates memory for a string entered by the user and finds its length using pointers.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n = 100;
+    char *str;
+    char *p;
+    int len = 0;
+
+    str = (char *)malloc(n * sizeof(char));
+    if (str == NULL)
+        return 1;
+
+    printf("Enter string: ");
+    scanf("%s", str);
+
+    p = str;
+    while (*p != '\0')
+    {
+        len++;
+        p++;
+    }
+
+    printf("Length: %d", len);
+
+    free(str);
+    return 0;
+}
