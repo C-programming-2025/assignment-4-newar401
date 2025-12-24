7d8ceaf49951d820455b51c64b7385fc6b425ae8
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-24 09:03:52 +0545

    Add Book structure and price filtering program
    Implement a C program to manage book details and filter by price.

--- src/q8.c
+++ src/q8.c
@@ -1,3 +1,40 @@
-// Write a C program that defines a structure Book with members: title, author, and price. 
-// Allow the user to enter details of n books and then display all books whose price is above a 
-// user-defined value.
+#include <stdio.h>
+
+struct Book
+{
+    char title[50];
+    char author[50];
+    float price;
+};
+
+int main()
+{
+    int n;
+    float limit;
+
+    printf("Enter number of books: ");
+    scanf("%d", &n);
+
+    struct Book b[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter title: ");
+        scanf("%s", b[i].title);
+
+        printf("Enter author: ");
+        scanf("%s", b[i].author);
+
+        printf("Enter price: ");
+        scanf("%f", &b[i].price);
+    }
+
+    printf("Enter price limit: ");
+    scanf("%f", &limit);
+
+    for (int i = 0; i < n; i++)
+        if (b[i].price > limit)
+            printf("\nTitle: %s\nAuthor: %s\nPrice: %.2f\n", b[i].title, b[i].author, b[i].price);
+
+    return 0;
+}
