b7dba4a43c44823648337d149364bb092a94c999
Aadarsha Shrestha <shresthaaadarsha4321@gmail.com>
2025-12-24 09:04:20 +0545

    Update q9.c

--- src/q9.c
+++ src/q9.c
@@ -1,2 +1,28 @@
-// Write a C program that uses a structure Point with members x and y, representing coordinates. 
-// Write a function that takes two Point structures and returns the distance between them.
+#include <stdio.h>
+#include <math.h>
+
+struct Point
+{
+    float x;
+    float y;
+};
+
+float distance(struct Point a, struct Point b)
+{
+    return sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+}
+
+int main()
+{
+    struct Point p1, p2;
+
+    printf("Enter x and y of first point: ");
+    scanf("%f %f", &p1.x, &p1.y);
+
+    printf("Enter x and y of second point: ");
+    scanf("%f %f", &p2.x, &p2.y);
+
+    printf("Distance: %.2f", distance(p1, p2));
+
+    return 0;
+}
